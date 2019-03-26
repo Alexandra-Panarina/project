9449f7b6eb9fb1e4d0322249ea41e1844274da70
Alexandra Panarina <shurik.97@bk.ru>
2019-03-26 15:09:34 +0300

    init

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -28,7 +28,7 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'railway_arrow':
+    if row_label == 'railway_arrows':
         return 1
     else:
         None
